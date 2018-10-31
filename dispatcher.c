36a831cf8b64ad2b0a43d3de35fc5f2b1388fee5
njvanhaute <njvanhaute@crimson.ua.edu>
2018-10-31 05:36:17 +0000

    Non-functional at the moment... always a good sign

--- dispatcher.c
+++ dispatcher.c
@@ -1,11 +1,20 @@
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "da.h"
+
+#define BUF_SIZE 1024
 
 typedef struct process_struct {
     int arrival_time;
     int priority;
     int proc_time;
 } process;
+
+process *new_proc(int, int, int);
+void display_proc(FILE *, void *);
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -20,14 +29,49 @@ int main(int argc, char **argv) {
         return 1;
     }
     
-    char line_buf[255];
-    while (fgets(line_buf, 255, dispatch_file)) {
-        char *tok = strtok(line_buf, ",");
-        while (tok) {
-            printf("%s ", tok);
-            tok = strtok(NULL, ",");
+    DA *proc_da = newDA(display_proc);
+    char line_buf[BUF_SIZE];
+    
+    while (fgets(line_buf, BUF_SIZE, dispatch_file)) {
+        int arrival_time, priority, proc_time; 
+        const int got = sscanf(line_buf, "%d,%d,%d", &arrival_time, &priority, &proc_time);
+        if (got != 3) {
+            fprintf(stderr, "error parsing file.\n");
+            return 1;
         }
-        printf("\n");
+        process *proc = new_proc(arrival_time, priority, proc_time);
+        insertDA(proc_da, proc);
     }
+
+    int curr_time = 0;
+    int proc_ptr = 0;
+    while (proc_ptr < sizeDA(proc_da)) {
+        process *curr_proc = (process *)getDA(proc_da, proc_ptr);
+        if (curr_proc->arrival_time == curr_time) {
+            pid_t child_pid = fork();
+            if (child_pid == 0) {
+                char *time_buf = malloc(sizeof(char) * sizeof(int) * 4 + 1);
+                sprintf(time_buf, "%d", curr_proc->proc_time);
+                char **proc_args = malloc(sizeof(char *));
+                proc_args[0] = time_buf;
+                execvp("./process", proc_args);
+            }
+        }
+        sleep(1);
+        curr_time++; 
+    }   
     return 0;
+}
+
+process *new_proc(int arrival_time, int priority, int proc_time) {
+    process *np = malloc(sizeof(process));
+    np->arrival_time = arrival_time;
+    np->priority = priority;
+    np->proc_time = proc_time;
+    return np;
+}
+
+void display_proc(FILE *fp, void *value) {
+    process *proc = (process *)value;
+    fprintf(fp, "{%d %d %d}", proc->arrival_time, proc->priority, proc->proc_time);
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 hostd: dispatcher.c sigtrap.c
-	gcc -g dispatcher.c -o dispatcher -Wall 
+	gcc -g dispatcher.c da.c -o dispatcher -Wall 
 	gcc -g sigtrap.c -o process -Wall
 
 .PHONY: clean
