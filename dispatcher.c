d198e5b15de4f630c5c91ebaa5b7d588aa182548
njvanhaute <njvanhaute@crimson.ua.edu>
2018-11-01 16:06:02 -0500

    Final

--- dispatcher.c
+++ dispatcher.c
@@ -5,8 +5,6 @@
 #include <signal.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include "da.h"
-#include "cda.h"
 
 #define BUF_SIZE 1024
 #define TIME_QUANTUM 1
@@ -21,6 +19,25 @@ typedef struct process_struct {
     pid_t pid;
 } process;
 
+struct da {
+    void **arr;
+    int size; // Indicates the number of elements stored in the array
+    int cap; // Indicates the capacity of the array
+    void (*display)(FILE *, void *);
+};
+
+typedef struct da DA;
+
+struct cda {
+    int size, cap;
+    int front; // Index indicating the front of the array. No need to store the
+    // back; we can compute it with the front and the size.
+    void **arr;
+    void (*display)(FILE *, void *);
+};
+
+typedef struct cda CDA;
+
 void startProcess(process *);
 void terminateProcess(process *);
 void suspendProcess(process *);
@@ -29,6 +46,35 @@ char *str_from_int(int);
 DA *get_procs_with_arrival_time(CDA *, int);
 process *new_proc(int, int, int);
 void display_proc(FILE *, void *);
+
+
+
+DA *newDA(void (*d)(FILE *,void *));
+void insertDA(DA *items,void *value);
+void *removeDA(DA *items);
+void unionDA(DA *recipient,DA *donor);
+void *getDA(DA *items,int index);
+void *setDA(DA *items,int index,void *value);
+void **extractDA(DA *items);
+int sizeDA(DA *items);
+void visualizeDA(FILE *fp,DA *items);
+void displayDA(FILE *fp,DA *items);
+
+
+
+CDA *newCDA(void (*d)(FILE *,void *));
+void insertCDAfront(CDA *items,void *value);
+void insertCDAback(CDA *items,void *value);
+void *removeCDAfront(CDA *items);
+void *removeCDAback(CDA *items);
+void unionCDA(CDA *recipient,CDA *donor);
+void *getCDA(CDA *items,int index);
+void *setCDA(CDA *items,int index,void *value);
+void **extractCDA(CDA *items);
+int sizeCDA(CDA *items);
+void visualizeCDA(FILE *,CDA *items);
+void displayCDA(FILE *,CDA *items);
+
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -218,4 +264,405 @@ process *new_proc(int arrival_time, int priority, int proc_time) {
 void display_proc(FILE *fp, void *value) {
     process *proc = (process *)value;
     fprintf(fp, "{%d %d %d}", proc->arrival_time, proc->priority, proc->proc_time);
+}
+
+static void CDAincreaseCap(CDA *items);
+static void CDAreduceCap(CDA *items);
+
+// Function: newCDA
+// Takes in a function pointer to display the objects stored in the array.
+// Returns the newly allocated CDA
+
+CDA *newCDA(void (*d)(FILE *, void *)) {
+    CDA *cda = (CDA *)malloc(sizeof(CDA));
+    assert(cda != 0);
+    cda->size = 0;
+    cda->front = 0;
+    cda->cap = 1;
+    cda->arr = (void **)malloc(cda->cap * sizeof(void *));
+    assert(cda->arr != 0);
+    cda->display = d;
+    return cda;
+}
+
+// Function: insertCDAfront
+// Takes in a CDA object and a void pointer.
+// Inserts the void pointer at the front of the contiguous region.
+
+void insertCDAfront(CDA *items, void *value) {
+    if (items->size == items->cap) {
+        CDAincreaseCap(items);
+    }
+    items->front--;
+    if (items->front < 0) {
+        items->front += items->cap;
+    }
+    assert(items->front < items->cap && items->front >= 0);
+    items->arr[items->front] = value;
+    items->size++;
+}
+
+// Function: insertCDAback
+// Takes in a CDA object and a void pointer.
+// Inserts the void pointer at the back of the contiguous region.
+
+void insertCDAback(CDA *items, void *value) {
+    if (items->size == items->cap) {
+        CDAincreaseCap(items);
+    }
+    int back = (items->front + items->size) % items->cap;
+    items->arr[back] = value;
+    items->size++;
+}
+
+// Function: removeCDAfront
+// Takes in a CDA object
+// Removes and returns the void pointer at the front of the contiguous region.
+
+void *removeCDAfront(CDA *items) {
+    assert(items->size > 0);
+    if (items->cap > 1 && (items->size - 1) / (float) items->cap < 0.25) {
+        CDAreduceCap(items);
+    }
+    void *front = items->arr[items->front];
+    items->front = (items->front + 1) % items->cap;
+    items->size--;
+    return front;
+}
+
+// Function: removeCDAback
+// Takes in a CDA object
+// Removes and returns the void pointer at the back of the contiguous region.
+
+void *removeCDAback(CDA *items) {
+    assert(items->size > 0);
+    if (items->cap > 1 && (items->size - 1) / (float) items->cap < 0.25) {
+        CDAreduceCap(items);
+    }
+    int b = (items->front - 1 + items->size) % items->cap;
+    void *back = items->arr[b];
+    items->size--;
+    return back;
+}
+
+// Function: unionCDA
+// Takes in recipient and donor CDAs.
+// Appends all the items in the donor array to the recipient array.
+// The donor array is empty after the union operation.
+
+void unionCDA(CDA *recipient, CDA *donor) {
+    for (int i = 0; i < donor->size; i++) {
+        int index = donor->front + i;
+        insertCDAback(recipient, donor->arr[index % donor->cap]);
+    }
+    while (donor->size != 0) {
+        removeCDAback(donor);
+    }
+}
+
+// Function: getCDA
+// Takes in a CDA object and an index
+// Returns the value at the specified index from the perspective of the user
+
+void *getCDA(CDA *items, int index) {
+    assert(index >= 0 && index < items->size);
+    return items->arr[(items->front + index) % items->cap];
+}
+
+// Function: setCDA
+// Takes in a CDA object, an index, and a void pointer.
+// Changes the value at the specified index to be that of the void pointer.
+// If a value is replaced, returns the replaced value. Otherwire, returns NULL
+
+void *setCDA(CDA *items, int index, void *value) {
+    assert(index >= -1 && index <= items->size);
+    void *replaced = 0;
+    if (index == items->size) {
+        insertCDAback(items, value);
+    }
+    else if (index == -1) {
+        insertCDAfront(items, value);
+    }
+    else {
+        replaced = items->arr[(items->front + index) % items->cap];
+        items->arr[(items->front + index) % items->cap] = value;
+    }
+    return replaced;
+}
+
+// Function: extractCDA
+// Takes in a CDA object
+// Returns the underlying C array shrunk to an exact fit.
+// The CDA object gets a new array of capacity 1 and size 0.
+
+void **extractCDA(CDA *items) {
+    if (items->size == 0) {
+        return 0;
+    }
+    void **arr = (void **)malloc(items->size * sizeof(void *));
+    assert(arr != 0);
+    for (int i = 0; i < items->size; i++) {
+        arr[i] = items->arr[(items->front + i) % items->cap];
+    }
+    free(items->arr);
+    items->size = 0;
+    items->cap = 1;
+    items->arr = (void **)malloc(items->cap * sizeof(void *));
+    assert(items->arr != 0);
+    return arr;
+}
+
+// Function: sizeCDA
+// Takes in a CDA object
+// Returns the size of the CDA
+
+int sizeCDA(CDA *items) {
+    return items->size;
+}
+
+// Function: visualizeCDA
+// Takes in a file pointer and a CDA object
+// Prints the contiguous region of the array enclosed with parentheses
+//     and separated by commas, followed by the size of the unfilled region
+//     enclosed in parentheses.
+
+void visualizeCDA(FILE *fp, CDA *items) {
+    displayCDA(fp, items);
+    fprintf(fp, "(%d)", items->cap - items->size);
+}
+
+// Function: displayCDA
+// Takes in a file pointer and a CDA object
+// Similar to the visualizeCDA function, except it doesn't print the size of
+//     the unfilled region.
+
+void displayCDA(FILE *fp, CDA *items) {
+    fprintf(fp, "(");
+    for (int i = items->front; i < items->front + items->size; i++) {
+        items->display(fp, items->arr[i % items->cap]);
+        if (i < items->front + items->size - 1) {
+            fprintf(fp, ",");
+        }
+    }
+    fprintf(fp, ")");
+}
+
+// Static function: increaseCap
+// Takes in a CDA object
+// Double the size of the CDA array, and frees the old one
+
+static void CDAincreaseCap(CDA *items) {
+    int oldFront = items->front;
+    int oldCap = items->cap;
+    items->cap *= 2;
+    void **newArr = (void **)malloc(items->cap * sizeof(void *));
+    assert(newArr != 0);
+    for (int i = 0; i < items->size; i++) {
+        newArr[i] = items->arr[(oldFront++) % oldCap];
+    }
+    free(items->arr);
+    items->arr = newArr;
+    items->front = 0;
+}
+
+// Static function: reduceCap
+// Takes in a CDA object.
+// Similar to the increaseCap function, except it halves the size of the array.
+
+static void CDAreduceCap(CDA *items) {
+    int oldFront = items->front;
+    int oldCap = items->cap;
+    items->cap /= 2;
+    void **newArr = (void **)malloc(items->cap * sizeof(void *));
+    assert(newArr != 0);
+    for (int i = 0; i < items->size; i++) {
+        newArr[i] = items->arr[(oldFront++) % oldCap];
+    }
+    free(items->arr);
+    items->arr = newArr;
+    items->front = 0;
+}
+
+static void DAincreaseCap(DA *items);
+static void DAreduceCap(DA *items);
+
+// Function: newDA
+// Takes in a callback function to display the value stored in the data structure
+// User calls this function to create a new dynamic array.
+// Returns the new dynamic array.
+
+DA *newDA(void (*d)(FILE *, void *)) {
+    DA *da = (DA *)malloc(sizeof(DA));
+    assert(da != 0);
+    da->size = 0;
+    da->cap = 1;
+    da->arr = (void **)malloc(da->cap * sizeof(void *));
+    assert(da->arr != 0);
+    da->display = d;
+    return da;
+}
+
+// Function: insertDA
+// Takes in a DA object and a void pointer.
+// User calls this function to insert a void pointer into to the leftmost
+//    unfilled slot of the dynamic array
+
+void insertDA(DA *items, void *value) {
+    if (items->size == items->cap) {
+        DAincreaseCap(items);
+    }
+    items->arr[(items->size)++] = value;
+}
+
+// Function: removeDA
+// Takes in a DA object
+// User calls this function to remove the rightmost item in the filled region
+//     of the dynamic array.
+// Returns the value removed.
+
+void *removeDA(DA *items) {
+    assert(items->size > 0);
+    void *tail = items->arr[--(items->size)];
+    if (items->cap > 1 && items->size / (float) items->cap < 0.25) {
+        DAreduceCap(items);
+    }
+    return tail;
+}
+
+// Function: unionDA
+// Takes in a DA recipient and a DA donor
+// User calls this function to move all items in the donor array to the
+//     recipient array.
+// After the union, the donor will be empty and the recipient will contain
+//     have the items of the donor appended.
+
+void unionDA(DA *recipient, DA *donor) {
+    for (int i = 0; i < donor->size; i++) {
+        insertDA(recipient, donor->arr[i]);
+    }
+    while (donor->size != 0) {
+        removeDA(donor);
+    }
+}
+
+// Function: getDA
+// Takes in a DA object and an index
+// User calls this function to get the value stored in the DA at the specified
+//     index.
+// Returns the specified void pointer
+
+void *getDA(DA *items, int index) {
+    assert(index >= 0);
+    assert(index < items->size);
+    return items->arr[index];
+}
+
+// Function: setDA
+// Takes in a DA object, an index, and a void pointer
+// User calls this function to change the value at the specified index.
+// Returns the replaced value if a replacement was made, or a null pointer if
+//     no replacement was made.
+
+void *setDA(DA *items, int index, void *value) {
+    assert(index >= 0 && index <= items->size);
+    void *replaced = 0;
+    if (index == items->size) {
+        insertDA(items, value);
+    }
+    else {
+        replaced = items->arr[index];
+        items->arr[index] = value;
+    }
+    return replaced;
+}
+
+// Function: extractDA
+// Takes in a DA object
+// Returns the underlying C array, shrunk to an exact fit.
+// DA object gets a new array of capacity 1 and size 0.
+
+void **extractDA(DA *items) {
+    if (items->size == 0) {
+        return 0;
+    }
+    void **arr = (void **)malloc(items->size * sizeof(void *));
+    assert(arr != 0);
+    for (int i = 0; i < items->size; i++) {
+        arr[i] = items->arr[i];
+    }
+    free(items->arr);
+    items->size = 0;
+    items->cap = 1;
+    items->arr = (void **)malloc(items->cap * sizeof(void *));
+    assert(items->arr != 0);
+    return arr;
+}
+
+// Function: sizeDA
+// Takes in a DA object
+// Returns the size of the array
+
+int sizeDA(DA *items) {
+    return items->size;
+}
+
+// Function: visualizeDA
+// Takes in a file pointer and a DA object
+// Prints out the filled region enclosed in brackets and separated by commas,
+//     followed by the size of the unfilled region enclosed in brackets.
+
+void visualizeDA(FILE *fp, DA *items) {
+    displayDA(fp, items);
+    fprintf(fp, "[%d]", items->cap - items->size);
+}
+
+// Function: displayDA
+// Takes in a file pointer and a DA object
+// Similar to the visualizeDA method, except the bracketed size of the unfilled
+//     region is not printed.
+
+void displayDA(FILE *fp, DA *items) {
+    fprintf(fp, "[");
+    for (int i = 0; i < items->size; i++) {
+        items->display(fp, items->arr[i]);
+        if (i < items->size - 1) {
+            fprintf(fp, ",");
+        }
+    }
+    fprintf(fp, "]");
+}
+
+// Static function: increaseCap
+// Takes in a DA object
+// Doubles the size of the array, copies all the old elements over, and frees
+//   the old array
+
+static void DAincreaseCap(DA *items) {
+    items->cap *= 2;
+    void **newArr = (void **)malloc(items->cap * sizeof(void *));
+    assert(newArr != 0);
+    for (int i = 0; i < items->size; i++) {
+        newArr[i] = items->arr[i];
+    }
+    free(items->arr);
+    items->arr = newArr;
+}
+
+// Static function: reduceCap
+// Takes in a DA object
+// Similar to increaseCap, but halves the size of the array instead of doubling.
+
+static void DAreduceCap(DA *items) {
+    items->cap /= 2;
+    void **newArr = (void **)malloc(items->cap * sizeof(void *));
+    assert(newArr != 0);
+    for (int i = 0; i < items->size; i++) {
+        newArr[i] = items->arr[i];
+    }
+    free(items->arr);
+    items->arr = newArr;
+}
+
+int testing(int a, int b) {
+    return a + b;
 }
